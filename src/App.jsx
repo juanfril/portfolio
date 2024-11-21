636d13b4f9b52de85835fa2a8256c57cd6e5a284
juanfril <juanfril@gmail.com>
2024-11-21 13:42:22 +0100

    Updates

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import { Layout } from './pages/Layout'
-
-export function App() {
-  return <Layout />
-}
--- src/components/Tags.jsx
+++
@@ -1,7 +0,0 @@
-export function Tags({ tag }) {
-  return (
-    <span className="rounded-full bg-emerald-400/10 bg-opacity-95 text-emerald-300 px-3 py-1 text-xs font-medium leading-5">
-      {tag}
-    </span>
-  )
-}
